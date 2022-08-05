09e5ef63bdf25d18a8ca232ea783309557da200c
Ibraheem Azam <ibraheemazam@gmail.com>
2022-08-05 10:43:23 -0700

    updated .env file with more variables for DB

--- DB/index.js
+++ DB/index.js
@@ -2,9 +2,9 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   user: process.env.DB_USER,
-  host: 'localhost',
-  database: 'atelier',
-  password: '',
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
   port: 5432
 });
 
