96d67cf69595e4c9d9cb7b337c865486784913b7
Ubuntu <ubuntu@ip-172-31-5-230.us-west-1.compute.internal>
2022-08-05 22:23:08 +0000

    added loader io verification to server

--- server/routes.js
+++ server/routes.js
@@ -3,6 +3,16 @@ const router = express.Router();
 const controllers = require('../DB/controllers/controllers.js');
 const handlers = require('./routehandlers/handlers.js')
 
+router.get('/loaderio-16c0dcbe1fe23c8766d06c7ca6c010e2', (req, res) => {
+	res.sendFile("/home/ubuntu/atelierAPI/loaderIOToken/loaderio-16c0dcbe1fe23c8766d06c7ca6c010e2.txt", null, (err) => {
+		if (err) {
+			next(err);
+		} else {
+			console.log("File sent:");
+		}
+	});
+});
+
 router.get('/products', handlers.products)
 
 router.get('/products/:product_id', handlers.product);
