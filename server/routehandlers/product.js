1e3beff3f40def415c6af21e1cb8984d320ab0e6
Ibraheem Azam <ibraheemazam@gmail.com>
2022-08-06 11:03:20 -0700

    changed get product back. Apparently queruy is not under 50 because of location

--- server/routehandlers/product.js
+++ server/routehandlers/product.js
@@ -3,11 +3,11 @@ const controllers = require('../../DB/controllers/controllers.js');
 const productHandler = (req, res) => {
   controllers.getProduct(req.params.product_id)
     .then((result) => {
-      // if (result.rows.length < 1) {
-      //   res.status(404).send('This product does not exist.')
-      // } else {
+      if (result.rows.length < 1) {
+        res.status(404).send('This product does not exist.')
+      } else {
         res.send(result.rows)
-      // }
+      }
     })
     .catch((err) => {
       console.log('Error sending product,:\n', err);
